00b9480fe13f04d689a104e6f7e8d023ab06c67d
Justin Romanowski <just.roman7@gmail.com>
2017-04-20 17:52:43 -0700

    fixed all spaceing issues

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 var animatePoints = function(points) {
-      var revealPoint = function(index) {
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.webkittransform = "scaleX(1) translateY(0)";
-    };
-    for (var i =0; i <= points.length; i++) {
-      revealPoint(i);
-    }
+  var revealPoint = function(index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.webkittransform = "scaleX(1) translateY(0)";
+  };
+  for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
+  }
 };
 
 window.onload = function () {
@@ -19,7 +19,7 @@ window.onload = function () {
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
   window.addEventListener('scroll', function(event) {
     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
+      animatePoints(pointsArray);
     }
   });
 };
