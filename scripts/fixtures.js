355602031e586f0abbd5218a13061c0edeed1477
Justin Romanowski <just.roman7@gmail.com>
2017-05-15 14:18:16 -0700

    finished checkpoint 19

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: ' Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    { title: 'Blue', duration: '4:26'},
+    { title: 'Green', duration: '3:14'},
+    { title: 'Red', duration: '5:01'},
+    { title: 'Pink', duration: '3:21'},
+    { title: 'Magenta', duration: '2:15'},
+  ]
+};
+
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: ' EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01'},
+    { title: 'Ring, ringm ring', duration: '5:01'},
+    { title: 'Fits in your pocket', duration: '3:21'},
+    { title: 'Can you hear me now?', duration: '3:14'},
+    { title: 'Wrong phone number', duration: '2:15'},
+  ]
+};
+
+var albumKnifeParty = {
+  title: 'Abandon Ship',
+  artist: 'Knife Party',
+  label: ' Earstorm',
+  year: '2014',
+  albumArtUrl: 'assets/images/album_covers/Knife_Party_-_Abandon_Ship.jpg',
+  songs: [
+    { title: 'Boss Mode', duration: '3:47'},
+    { title: '404', duration: '4:59'},
+    { title: 'Give it up', duration: '4:11'},
+    { title: 'Red Dawn', duration: '6:07'},
+    { title: 'Begin Again', duration: '5:55'},
+  ]
+};
